d809d5fe2eca4e0da376be29c69c33cc1fb406d2
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 19:20:49 -0300

    Modificando um array

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,8 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+
+let frutasModificado = frutas.map((x, i) => {
+    x = x.toLowerCase();
+    return `${i} - ${x[0].toUpperCase()}${x.substr(1)}`;
+});
+
+console.log(frutasModificado);
