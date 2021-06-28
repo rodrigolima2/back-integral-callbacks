513c5f2b380587f4e0ed044aa96748fa3982542e
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 19:56:37 -0300

    Temporizador de jogos

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,16 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+
+let contagem = 0;
+
+function imprimirJogador() {
+
+    if (contagem < jogadores.length) {
+        console.log(jogadores[contagem]);
+        contagem++;
+    } else {
+        console.log("A rodada terminou!");
+        clearInterval(intervalId);
+    }
+}
+
+const intervalId = setInterval(imprimirJogador, (10 / jogadores.length) * 1000);
