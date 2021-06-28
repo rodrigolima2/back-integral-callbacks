996bc2c315973ece6e3de3e2acb23bb13dae84fe
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 18:16:37 -0300

    Filtro de caracteres

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,6 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+let cidadesFiltradas = cidades.filter(x => x.length <= 8);
+cidadesFiltradas = cidadesFiltradas.join(", ");
+
+console.log(cidadesFiltradas);
